d369836fa1ee41c5e37375932b4ee1940f8789d1
Lu Valcke <lfvalcke@gmail.com>
2021-06-23 15:35:54 -0500

    Implmentación función botones comprar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+/*function btnComprar(){
+    document.getElementById("Bienvenida").style.display="none";
+    document.getElementById("datos").removeAttribute("hidden");
+    document.getElementById("datos").style.display="block";
+}
+
+//Averiguar cómo podríamos evitar tener que hacer ID por cada botón
+document.getElementById("btnUno").addEventListener("click", btnComprar); 
+document.getElementById("btnDos").addEventListener("click", btnComprar);*/
+
+document.querySelectorAll('.btn').forEach(btn => btn.addEventListener('click', e => {
+    document.getElementById('Bienvenida').setAttribute('hidden','');
+    document.getElementById('datos').removeAttribute('hidden');
+}))
